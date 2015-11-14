ee1978abcf1c96a006264f0877454035414c4279
mayago <golkondamanoj@gmail.com>
2015-11-14 16:52:09 +0530

    Submit Commit

--- src/removeSpaces.cpp
+++ src/removeSpaces.cpp
@@ -11,6 +11,18 @@ ERROR CASES: Return '\0' for invalid inputs.
 NOTES: Don't create new string.
 */
 
+#define NULL 0
+
 char removeSpaces(char *str) {
-	return '\0';
+	if (str == NULL || str[0] == '\0')
+		return '\0';
+	int i = 0, j = 0;
+	while (str[j] != '\0')
+	{
+		if (str[j] != ' ')
+			str[i++] = str[j];
+		j++;
+	}
+	str[i] = '\0';
+	return *str;
 }
