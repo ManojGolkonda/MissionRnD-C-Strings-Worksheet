a39c4103dfa8e7b23c9291300ff23f5d6c20880b
mayago <golkondamanoj@gmail.com>
2015-11-14 16:30:58 +0530

    Submit Commit

--- src/KthIndexFromEnd.cpp
+++ src/KthIndexFromEnd.cpp
@@ -1,4 +1,4 @@
-/*
+/*nu
 OVERVIEW: Given a string, return the Kth index from the end of the string.
 E.g.: Input: "qwertyui", 3. Output: 't' (index starting from zero).
 
@@ -10,8 +10,15 @@ ERROR CASES: Return '\0' for invalid inputs.
 
 NOTES:
 */
+#define NULL 0
 
 char KthIndexFromEnd(char *str, int K) {
-	if (str[0] == 0 || str == nullptr)
+	if (str == NULL || str[0] == '\0' || K < 0)
 		return '\0';
+	int i = 0;
+	while (str[i] != '\0')
+		i++;
+	if (K < i)
+		return str[i - K - 1];
+	return '\0';
 }
